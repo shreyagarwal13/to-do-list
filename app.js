3a488c602803bfc443f3c1f86912f4bdd48c272a
Shrey Agarwal <shreyagarwal406@gmail.com>
2021-02-03 22:59:38 +0530

    add procfile gitignore

--- app.js
+++ app.js
@@ -143,8 +143,11 @@ app.post("/switch", function(req,res){
 });
 
 
+let port = process.env.PORT;
+if(port==NULL || port ==""){
+  port = 3000;
+}
 
-
-app.listen(3000, function(){
-  console.log("The server has started on port 3000");
+app.listen(port, function(){
+  console.log("Server has started successfully");
 });
